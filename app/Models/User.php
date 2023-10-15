adadab3f02ecd34d227c06240f4310e6273940cc
DESKTOP-3HQJF36\joken <calibosokevin@gmail.com>
2023-10-15 09:06:02 +0800

    added fields in the fillable section of User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +23,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'firstname',
+        'lastname',
+        'username',
+        'contact',
+        'birthday',
+        'address',
+        'phonenumber'
     ];
 
     /**
