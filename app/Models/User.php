c575d69d93d86fe5fe2c9ecf4c27ea995435d82f
DESKTOP-3HQJF36\joken <calibosokevin@gmail.com>
2023-10-15 14:55:16 +0800

    declared an accessor to convert birthday for better readability

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Attribute;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -51,4 +52,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function birthday(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => (Carbon::parse($value)->format('F j, Y'))
+        );
+    }
 }
