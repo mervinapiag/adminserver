6f812daf2ea52c6cb406428a60f4ed6275bbf584
Kevin Caliboso <joken767@gmail.com>
2023-11-05 17:13:07 +0800

    creating payment option functionality

---
+++ app/Http/Controllers/PaymentOptionController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers;
+use App\Http\Requests\PaymentOptionRequest;
+use App\Http\Resources\PaymentOptionCollection;
+use App\Http\Resources\PaymentOptionResource;
+use App\Models\PaymentOption;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+
+class PaymentOptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new PaymentOptionCollection(PaymentOption::paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PaymentOptionRequest $request)
+    {
+        $data = $request->all();
+        try {
+            DB::beginTransaction();
+            $data = PaymentOption::create($data);
+            DB::commit();
+
+            return Helpers::returnJsonResponse(config('constants.RECORD_CREATED'), Response::HTTP_CREATED, new PaymentOptionResource($data));
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return Helpers::returnJsonResponse(config('constants.RECORD_ERROR'), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PaymentOption $payment_option)
+    {
+        return Helpers::returnJsonResponse(config('constants.RECORD_INFO'), Response::HTTP_OK, new PaymentOptionResource($payment_option));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(PaymentOptionRequest $request, string $id)
+    {
+        $data = $request->all();
+        $discount = PaymentOption::find($id);
+
+        try {
+            DB::beginTransaction();
+            $discount->update($data);
+            DB::commit();
+
+            return Helpers::returnJsonResponse(config('constants.RECORD_UPDATED'), Response::HTTP_ACCEPTED, new PaymentOptionResource($discount));
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return Helpers::returnJsonResponse(config('constants.RECORD_ERROR'), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $payment_option = PaymentOption::find($id);
+        try {
+            DB::beginTransaction();
+            $payment_option->delete();
+            DB::commit();
+
+            return Helpers::returnJsonResponse(config('constants.RECORD_DELETED'), Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return Helpers::returnJsonResponse(config('constants.RECORD_ERROR'), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
