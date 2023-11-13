efdddfb12c8ff2efcb4a81414eaf09bd25116660
mervin-work <mervinapiag.work@gmail.com>
2023-11-13 12:39:06 +0800

    Remove payment_id, order_id on orders, payment tables and add etc.

---
+++ database/migrations/2023_11_13_043331_add_payment_id_fk_on_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('payment_id')->after('shoe_id');
+            $table->index('payment_id');
+            $table->foreign('payment_id')->references('id')->on('payments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['payment_id']);
+            $table->dropIndex(['payment_id']);
+            $table->dropColumn('payment_id');
+        });
+    }
+};
