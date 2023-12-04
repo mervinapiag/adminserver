f5b191fb4044136c74da2d3042fb9ac68a0f8e05
bryanjamesdelaluya <delaluya@gmail.com>
2023-12-04 14:36:04 +0800

    fixes for fitlers

--- app/Models/productColor.php
+++ app/Models/productColor.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class productColor extends Model
+class ProductColor extends Model
 {
     use HasFactory, SoftDeletes;
 
--- app/Models/productHasType.php
+++ app/Models/productHasType.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class productHasType extends Model
+class ProductHasType extends Model
 {
     use HasFactory, SoftDeletes;
 
