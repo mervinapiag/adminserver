921506f1c606efe5b45aefffe379e8d5b8224ce7
bryanjamesdelaluya <delaluya@gmail.com>
2023-12-13 13:04:14 +0800

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'otp'
     ];
 
     protected $with = ['userRole', 'addresses'];
